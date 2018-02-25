2bb4471f55b730e766527fb1557474164bd907e7
nyhu <toussaint.boos@gmail.com>
2018-02-25 11:38:30 +0100

    minor changes

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -31,6 +31,10 @@ export class GalleryComponent implements OnInit {
     return 0.7 / ((currentElement - currentThumb) * (currentElement - currentThumb));
   }
 
+  public getHumanPath() {
+    return this.route.split("/").join(" ");
+  }
+
   @HostListener('window:scroll', [])
   onWindowScroll() {
     this.currentElement = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
